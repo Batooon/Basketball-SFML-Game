6120924d233d64762cb11d6ab0a497b799b0996a
Anton <rozum.ant@gmail.com>
2019-12-10 22:10:54 +0200

    Initial commit

---
+++ Game/Ball.cs
@@ -0,0 +1,21 @@
+﻿using SFML.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game
+{
+    class Ball
+    {
+        const string CONTENT_DIR = "..\\Content\\";
+
+        public Texture ballTexture;
+
+        public Ball()
+        {
+            ballTexture = new Texture(CONTENT_DIR + "ball.png");
+        }
+    }
+}
---
+++ Game/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SFML.Graphics;
+
+namespace Game
+{
+    class Program
+    {
+        static RenderWindow win;
+
+        static void Main(string[] args)
+        {
+            win = new RenderWindow(new SFML.Window.VideoMode(800, 600), "Basketball");
+            win.SetVerticalSyncEnabled(true);
+
+            win.Closed += Win_Closed;
+
+            //Загрузка контентаdsw
+        }
+
+        private static void Win_Closed(object sender, EventArgs e)
+        {
+            win.Close();
+        }
+    }
+}
