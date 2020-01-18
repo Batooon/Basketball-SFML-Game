2a49e2d1fa08b29005cb3e42ed43110ef1f9dfa9
Anton <rozum.ant@gmail.com>
2020-01-18 20:13:48 +0200

    Added input manager

---
+++ Game/InputManager.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SFML.System;
+using SFML.Window;
+
+namespace Game
+{
+    public class InputManager
+    {
+        bool wasPressed = false;
+        public Vector2i mousePos = new Vector2i();
+
+        public void RefreshInput(ref bool isEndGame)
+        {
+            if (Keyboard.IsKeyPressed(Keyboard.Key.Escape))
+                isEndGame = true;
+
+            if (Mouse.IsButtonPressed(Mouse.Button.Left))
+            {
+                if (wasPressed)
+                    return;
+
+                wasPressed = true;
+
+                mousePos = Mouse.GetPosition();
+            }
+            else
+                wasPressed = false;
+        }
+    }
+}
